cfe69e2c29090c30ebec14c6042d1956bdddadd0
Samuele Vianello <samuele.vianello2002@gmail.com>
2023-12-31 13:25:11 +0100

    test logo

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
   page_title="Policumbent",
-  page_icon="https://blog.smartcae.com/admin/wp-content/uploads/2.png", 
+  page_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLBnwH3bm6RwJvsl1-w4PDKxydP6wUIJNDs9pMaI1lpw&s", 
 )
 #code to hide streamlit normal view
 hide_st_style = """
