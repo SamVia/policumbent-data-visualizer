789be6a699db965bd097f372cababe0a750f9204
Samuele Vianello <samuele.vianello2002@gmail.com>
2023-12-31 13:23:58 +0100

    added logo and basic graphics

--- pages/2_⚙️_Settings.py
+++ pages/2_⚙️_Settings.py
@@ -1,2 +1,16 @@
 import streamlit as st
+
+st.set_page_config(
+  page_title="Policumbent",
+  page_icon="https://blog.smartcae.com/admin/wp-content/uploads/2.png", 
+)
+#code to hide streamlit normal view
+hide_st_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </stile>
+"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
 st.write("hello")
