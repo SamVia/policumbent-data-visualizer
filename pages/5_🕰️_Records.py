53d3e87030e0a9b8033fd4de34e9b3857227a6f6
Samuele Vianello <samuele.vianello2002@gmail.com>
2024-04-11 18:34:41 +0200

    added utils files

---
+++ pages/5_🕰️_Records.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+  page_title="Policumbent",
+  page_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLBnwH3bm6RwJvsl1-w4PDKxydP6wUIJNDs9pMaI1lpw&s", 
+)
+#code to hide streamlit normal view
+hide_st_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </stile>
+"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
+st.write("hello")
