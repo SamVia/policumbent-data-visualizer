00024ea27281745fcc62445c3c6ac59fc2d4c497
Samuele Vianello <samuele.vianello2002@gmail.com>
2024-04-14 16:43:31 +0200

    added first read func

--- pages/3_📺_Live.py
+++ pages/3_📺_Live.py
@@ -39,8 +39,31 @@ if "read_ids" not in st.session_state:
 if "same_read" not in st.session_state:
 	#counter to avoid making new queries if the data stream is over
 	st.session_state.same_read = 0
-	
-	
+if "First" not in st.session_state:
+	st.session_state.First = True	
+
+def firstRead(db):
+	'''
+	reads the db once at the start, prevents a loss of initial data streamed, it is added in bulk not real time.
+	'''
+	try:
+		data = db.collection("test1").order_by("id",direction=firestore.Query.ASCENDING).get()
+		if len(data)>0:
+			current_read = []
+			
+			for r in data:
+				r_td = r.to_dict() #transform r object in data to a dict
+				current_read.append(r_td["id"])
+
+				if r_td["id"] not in st.session_state.read_ids:
+					#only if the id is not yet in the list of the previously read ids it is then added to the list of known data
+					st.session_state.collection.insert(0, r_td)
+					st.session_state.average += r_td["velocity"]
+		st.session_state.read_ids = [current_read[-1],current_read[-2],current_read[-3]]
+		return current_read
+	except:
+		pass
+
 def cleanDB(db):
 	'''
 	erases all data in the database, used for testing purposes
@@ -73,20 +96,44 @@ def templateDB(db):
 		return "template created"
 	except:
 		pass
+def collection(delay):
+	"""
+	writes the table + average if possible
+	"""
+	df = pd.DataFrame(st.session_state.collection)
+	col1, col2 = st.columns(2)
+	col1.dataframe(df, use_container_width=True, hide_index=True)
+	if len(st.session_state.collection) > 0:
+		#check if it possible to form an average and current velocity
+		speed = st.session_state.collection[0]["velocity"]
+		col2.write(f"current speed: {speed}m/s")
+		col2.divider()
+		col2.write(f"average speed: {round(st.session_state.average/len(st.session_state.collection),2)}m/s")
+	if delay>0:
+		time.sleep(delay)
+		col1.empty()
+
+
 
 db = connect_to_db() # establish connection
+
 if st.button("delete"):
 		st.write(cleanDB(db))
 
 if st.button("create"):
 	#temp
 	st.write(templateDB(db))
-
+if st.session_state.First:
+	first = firstRead(db)
+	collection(0.2)
+	
+	st.session_state.First = False
+	st.rerun()
 #if number of same instances is less than 100 (20s) execute queries else idle 
-if st.session_state.same_read < 100:
+while st.session_state.same_read < 100:
 	#query for data: gets data from "insert_name" in descending order (Higher to Lower), then limited to 6
-	#from testing 6 seems the right amount, with 5 there seems to be around 0.6% loss of ids with 6 out of 3 tests there has been 0 loss
-	data = db.collection("test1").order_by("id",direction=firestore.Query.DESCENDING).limit(6).get()
+	#from testing 3 seems the right amount, with 2 there seems to be around 0.6% loss of ids, with 3 out of 5 tests there has been 0 loss
+	data = db.collection("test1").order_by("id",direction=firestore.Query.DESCENDING).limit(3).get()
 	#check on data len -> if 0 there has been 0 matches
 
 	if len(data)>0:
@@ -106,27 +153,11 @@ if st.session_state.same_read < 100:
 			st.session_state.read_ids = current_read
 			st.session_state.same_read = 0
 
-	df = pd.DataFrame(st.session_state.collection)
-	col1, col2 = st.columns(2)
-	col1.dataframe(df, use_container_width=True, hide_index=True)
-	if len(st.session_state.collection) > 0:
-		#check if it possible to form an average and current velocity
-		speed = st.session_state.collection[0]["velocity"]
-		col2.write(f"current speed: {speed}m/s")
-		col2.divider()
-		col2.write(f"average speed: {round(st.session_state.average/len(st.session_state.collection),2)}m/s")
-	time.sleep(0.2)
-
-	col1.empty()
+	collection(0.2)
+	
+	
+	
 	st.rerun()
 else:
 	st.write("final stage")
-	df = pd.DataFrame(st.session_state.collection)
-	col1, col2 = st.columns(2)
-	col1.dataframe(df, use_container_width=True, hide_index=True)
-	if len(st.session_state.collection) > 0:
-		#check if it possible to form an average and current velocity
-		speed = st.session_state.collection[0]["velocity"]
-		col2.write(f"current speed: {speed}m/s")
-		col2.divider()
-		col2.write(f"average speed: {round(st.session_state.average/len(st.session_state.collection),2)}m/s")
+	collection()
