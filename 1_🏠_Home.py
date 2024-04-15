b7c8c3207dfd3e7c9b709e62787ae638fe8cdd6e
Samuele Vianello <samuele.vianello2002@gmail.com>
2024-04-15 14:55:58 +0200

    test cloning db

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+import git
+import os
 st.set_page_config(
   page_title="Policumbent",
   page_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLBnwH3bm6RwJvsl1-w4PDKxydP6wUIJNDs9pMaI1lpw&s", 
@@ -13,5 +14,12 @@ hide_st_style = """
     </stile>
 """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-
-st.write("home")
+@st.cache_resource(ttl=10000)
+def cloneDB():
+  username= "SamVia"
+  password = "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"
+  remote = f"https://{username}:{password}@github.com/SamVia/test_python"
+  git.Repo.clone_from(remote, r"/mount/src/policumbent-data-visualizer")
+  return 0
+result = cloneDB()
+st.write(os.scandir())
