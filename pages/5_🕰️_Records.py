0cc9cecfc0a40c0713cfef0d13e4e811227d5283
Samuele Vianello <samuele.vianello2002@gmail.com>
2024-04-15 15:50:37 +0200

    test on committed database

--- pages/5_🕰️_Records.py
+++ pages/5_🕰️_Records.py
@@ -19,7 +19,7 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 # @st.cache_resource
 def connect_to_db():
 # Connect to SQLite database
-  return sqlite3.connect('test.db')
+  return sqlite3.connect('/test_python/test.tb')
 
 # List of tables in the database
 @st.cache_data()
