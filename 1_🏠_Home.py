211629c392b5dde48c6a3dc151b6a8347d4b8856
Samuele Vianello <samuele.vianello2002@gmail.com>
2024-04-15 22:15:21 +0200

    t

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -25,21 +25,21 @@ result = cloneDB()
 
 def check_file_permissions(file_path):
    if os.access(file_path, os.R_OK):
-      print(f"Read permission is granted for file: {file_path}")
+      st.write(f"Read permission is granted for file: {file_path}")
    else:
-      print(f"Read permission is not granted for file: {file_path}")
+      st.write(f"Read permission is not granted for file: {file_path}")
     
    if os.access(file_path, os.W_OK):
-      print(f"Write permission is granted for file: {file_path}")
+      st.write(f"Write permission is granted for file: {file_path}")
 
    else:
       print(f"Write permission is not granted for file: {file_path}")
     
    if os.access(file_path, os.X_OK):
-      print(f"Execute permission is granted for file: {file_path}")
+      st.write(f"Execute permission is granted for file: {file_path}")
    else:
 
-      print(f"Execute permission is not granted for file: {file_path}")
+      st.write(f"Execute permission is not granted for file: {file_path}")
 
 st.write(os.path.realpath("test_python/test.tb"))
 check_file_permissions(os.path.realpath("test_python/test.tb"))
