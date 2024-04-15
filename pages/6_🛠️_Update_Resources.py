53e3cc1afb3e75549f6da9fb4bb8311870dfec9c
Samuele Vianello <samuele.vianello2002@gmail.com>
2024-04-15 13:13:23 +0200

    test on updating db with git

--- pages/6_🛠️_Update_Resources.py
+++ pages/6_🛠️_Update_Resources.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import os
 st.set_page_config(
   page_title="Policumbent",
   page_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLBnwH3bm6RwJvsl1-w4PDKxydP6wUIJNDs9pMaI1lpw&s", 
@@ -12,6 +12,23 @@ hide_st_style = """
     header {visibility: hidden;}
     </stile>
 """
-st.markdown(hide_st_style, unsafe_allow_html=True)
-st.write("hello")
+def updateDB():
+    import git
+    
+    repo = git.Repo(r"C:\Users\ACER\Desktop\testa\test_python")
+    username= "SamVia"
+    password = "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"
+    remote = f"https://{username}:{password}@github.com/SamVia/test_python"
+    
+    repo.git.add(os.path.realpath("test.db"))
+    repo.index.commit("pushed db")
 
+    origin = repo.remote(name="origin")
+    origin.push()
+
+
+st.markdown(hide_st_style, unsafe_allow_html=True)
+st.write("update database:")
+if st.button("update"):
+    updateDB()
+st.write(os.path.realpath("test.db"))
