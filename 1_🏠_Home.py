5510b3b993599eee20e467d06ac9a3fe7003364b
Samuele Vianello <samuele.vianello2002@gmail.com>
2024-04-30 14:58:34 +0200

    check on possibility on cloning

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import git
 import os
+import base64
 st.set_page_config(
   page_title="Policumbent",
   page_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLBnwH3bm6RwJvsl1-w4PDKxydP6wUIJNDs9pMaI1lpw&s", 
@@ -13,62 +14,54 @@ hide_st_style = """
     header {visibility: hidden;}
     </stile>
 """
+
+
 st.markdown(hide_st_style, unsafe_allow_html=True)
 st.write("home page")
-# @st.cache_resource(ttl=10000)
-# def cloneDB():
-#   username= "SamVia"
-#   password = "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"
-#   remote = f"https://{username}:{password}@github.com/SamVia/test_python"
-#   git.Repo.clone_from(remote, r"/mount/src/policumbent-data-visualizer/database")
-#   return 0
-# result = cloneDB()
 
-# def check_file_permissions(file_path):
-#    if os.access(file_path, os.R_OK):
-#       st.write(f"Read permission is granted for file: {file_path}")
-#    else:
-#       st.write(f"Read permission is not granted for file: {file_path}")
-    
-#    if os.access(file_path, os.W_OK):
-#       st.write(f"Write permission is granted for file: {file_path}")
 
-#    else:
-#       print(f"Write permission is not granted for file: {file_path}")
-    
-#    if os.access(file_path, os.X_OK):
-#       st.write(f"Execute permission is granted for file: {file_path}")
-#    else:
+pat = st.secrets["pat"]
+repo_dir = "/mount/src/test_python/database"
+username = "SamVia"
+repo_name = "Policumbent-Database"
+remote = f"https://{username}:{pat}@github.com/{username}/{repo_name}.git"
 
-#       st.write(f"Execute permission is not granted for file: {file_path}")
+def decode_base64_file(database_path):
+    # Read the base64-encoded content from the input file
+    try:
+        with open(database_path, "rb") as file:
+            encoded_content = file.read()
 
-# # st.write(os.path.realpath("test_python/test.tb"))
-# # check_file_permissions(os.path.realpath("test_python/test.tb"))
-# # os.chmod("test_python/test.tb", 0o777)
-# # check_file_permissions(os.path.realpath("test_python/test.tb"))
+        # Decode the base64-encoded content
+        decoded_content = base64.b64decode(encoded_content)
 
-# # Assuming you are currently in the 'START' directory
-# start_directory = os.getcwd()
+        # Write the decoded content to the output file
+        with open(database_path, "wb") as file:
+            file.write(decoded_content)
+    except:
+        st.write("there has been an error reading the file")
 
-# # Navigate to the 'REPO' directory within the 'START' directory
-# repo_directory = os.path.join(start_directory, "database/test_python")
-# full_file_path = ""
-# # Check if the 'REPO' directory exists
-# if os.path.exists(repo_directory):
-#     # Change the current working directory to the 'REPO' directory
-#     os.chdir(repo_directory)
 
-#     # Open and read the file 'FILE' within the 'REPO' directory
-#     file_path = "test.tb"
-#     full_file_path = os.path.join(repo_directory, "test.tb")
+try:
+    if os.path.isdir(repo_dir):
+        # If the directory already exists, just pull the changes
+        st.write("pulling")
+        repo = git.Repo(repo_dir)
+        repo.remotes.origin.pull()
+        
+    else:
+        #If the directory doesn't exist, clone the repository
+        st.write("cloning")
+        git.Repo.clone_from("https://github.com/SamVia/test_python.git", repo_dir, branch='master', depth=1, auth=("token", pat))
+        repo = git.Repo(repo_dir)
+except: pass
 
-#     # Check if the file exists
-#     if os.path.exists(full_file_path):
-#         # Open and read the file
-#       check_file_permissions(full_file_path)
-#     else:
-#         print("File not found.")
-# else:
-#     print("Repository directory not found.")
-# os.chmod(full_file_path, 0o777)
-# check_file_permissions(full_file_path)
+decode_base64_file(database_path=r'/mount/src/policumbent-data-visualizer/test.db')
+
+
+#change current working directory to where the database is:
+os.chdir("/mount/src/test_python/database")
+#change permissions to database file and folder#
+os.chmod("/mount/src/test_python/database/test.db", 0o777)
+os.chmod("/mount/src/test_python/database", 0o777)
+os.chdir("/mount/src/test_python")
