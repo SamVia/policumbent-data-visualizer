87df6c700b643d94c087c1a0a8798dd4fbf190fc
Samuele Vianello <samuele.vianello2002@gmail.com>
2024-05-01 16:30:04 +0200

    fix on test db

--- pages/6_🛠️_Update_Resources.py
+++ pages/6_🛠️_Update_Resources.py
@@ -44,7 +44,7 @@ def fillDB():
     day = random.randint(0,31)
     timed = datetime.now()
     table_name =  f"_{timed.year+5000}_{timed.month+1}_{day}"
-    db = r".\test.db"
+    db = r".\new_db.db"
     conn = create_connection(db)
 
 
